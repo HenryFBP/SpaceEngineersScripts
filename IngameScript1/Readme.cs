02db1f7915e433f2f0fb6831e239fb8e566ba701
Henry Post <HenryFBP@gmail.com>
2017-12-24 17:57:33 -0600

    update readme
    update readme to explain to users that things prefixed with "U_" are
    meant to be edited by them.

--- IngameScript1/Readme.cs
+++ IngameScript1/Readme.cs
@@ -1,8 +1,13 @@
-﻿/*
- *   R e a d m e
- *   -----------
+﻿/*  
+ *  /-------------\
+ *  | R e a d m e |
+ *  \-------------/
  * 
  *   In this file you can include any instructions or other comments you want to have injected onto the 
  *   top of your final script. You can safely delete this file if you do not want any such comments.
  * 
+ *  Hi! A few tips:
+ * 
+ *      - Modify variables prefixed with 'U', that means that I mean for end-users and non-coders to mess with them.
+ *      
  */
